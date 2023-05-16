5431f7fd4949e07f2c77af6669693e0e410eaffa
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-05-16 22:28:57 +0900

    add certify, activity, values-ko

---
+++ app/README.md
@@ -0,0 +1,9 @@
+```text
+개발 계획서
+
+[TODO List]
+- FirebaseStorage연동
+- Android Self DB이용(파일 경로, 파일 다운로드 확인용도)
+- 다운로드 받은 음원 파일 재생
+- 모바일 화면을 OFF한 상태에서도 음원이 재생되도록 구현
+```
---
+++ app/src/main/java/com/b3lon9/pungmoodlight/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.b3lon9.pungmoodlight
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
