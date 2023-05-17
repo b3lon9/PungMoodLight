bf0b5e284d2e3b6793e34dbd5a91e45e36d66cd6
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-05-17 11:57:46 +0900

    setting firebase

--- app/src/main/java/com/b3lon9/pungmoodlight/MainActivity.kt
+++ app/src/main/java/com/b3lon9/pungmoodlight/MainActivity.kt
@@ -2,11 +2,20 @@ package com.b3lon9.pungmoodlight
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.b3lon9.nlog.NLog
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var storage:FirebaseStorage
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        storage = Firebase.storage
+
+        firebaseInit()
     }
 
     override fun onResume() {
@@ -19,5 +28,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
+    }
+
+    private fun firebaseInit() {
+        var storageRef = storage.reference
+
+        // download file
+        val pathReference = storageRef.child("bonfire/bonfire1.wav")
     }
 }
